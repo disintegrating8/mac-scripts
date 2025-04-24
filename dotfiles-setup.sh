65d2d3d94318d5bcc7ce4ab9c4ab638e548394ed
disintegrating8 <disintegrating8@pm.me>
2025-04-24 16:27:28 +0900

    Create dotfiles-setup.sh

---
+++ dotfiles-setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ORIGINAL_DIR=$(pwd)
+REPO_URL="https://github.com/disintegrating8/dotfiles"
+REPO_NAME="dotfiles"
+
+is_stow_installed() {
+  brew list --formula | grep -qx "stow"
+}
+
+if ! is_stow_installed; then
+  echo "Install stow first"
+  exit 1
+fi
+
+cd ~
+
+# Check if the repository already exists
+if [ -d "$REPO_NAME" ]; then
+  echo "Repository '$REPO_NAME' already exists. Skipping clone."
+else
+  echo "Cloning repository..."
+  git clone "$REPO_URL"
+fi
+
+# Check if the clone was successful
+if [ $? -eq 0 ]; then
+  cd "$REPO_NAME"
+  echo "Stowing configurations..."
+  stow zsh
+  stow nvim
+else
+  echo "Failed to clone the repository."
+  exit 1
+fi
