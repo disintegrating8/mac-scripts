068a582919c070c9d3e9eedb313bb3eca057c7d7
disintegrating8 <disintegrating8@pm.me>
2025-04-24 21:20:04 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -49,4 +49,8 @@ install_casks "${FONTS[@]}"
 curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 (git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
 
+# Run dotfiles-setup
+echo "Installing dotfiles"
+. dotfiles-setup.sh
+
 echo "Setup complete!"
