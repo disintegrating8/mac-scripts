31e5e8175b7c2d3ce76bf7958e4f22d0b62dc796
disintegrating8 <disintegrating8@pm.me>
2025-04-24 21:02:53 +0900

    Update tiling.sh

--- tiling.sh
+++ tiling.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-ORIGINAL_DIR=$(pwd)
-REPO_URL="https://github.com/disintegrating8/dotfiles"
-REPO_NAME="dotfiles"
-
 echo "Installing Dependencies"
 install_packages "${TILING[@]}"
 
@@ -13,9 +9,15 @@ curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.
 (git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
 
 echo "Cloning Config"
-git clone "$REPO_URL"
-#mv $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
-#mv /tmp/dotfiles/.config/sketchybar $HOME/.config/sketchybar
-#rm -rf /tmp/dotfiles
+git clone https://github.com/disintegrating8/dotfiles.git /tmp/dotfiles
+mv $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
+mv /tmp/dotfiles/sketchybar/.config/sketchybar $HOME/.config/sketchybar
+mv /tmp/dotfiles/borders/.config/borders $HOME/.config/borders
+mv /tmp/dotfiles/yabai/.config/yabai $HOME/.config/yabai
+mv /tmp/dotfiles/skhd/.config/skhd $HOME/.config/skhd
+rm -rf /tmp/dotfiles
 
-brew services restart sketchybar
+brew services start skhd
+brew services start yabai
+brew services start sketchybar
+brew services start borders
