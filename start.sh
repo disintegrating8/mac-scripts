ae0c4487ff3ea16e584d4d11c3c083fc6de6fc76
disintegrating8 <disintegrating8@pm.me>
2025-04-24 23:43:25 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # Clone the repo
-git clone https://github.com/disintegrating8/mac-scripts.git
+git clone https://github.com/disintegrating8/mac-scripts.git ~/mac-scripts
 cd mac-scripts
 chmod +x run.sh
 ./run.sh
