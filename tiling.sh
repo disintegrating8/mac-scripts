c18c6cd26eae1a2677ce99d4ab2d1a03597efb96
disintegrating8 <disintegrating8@pm.me>
2025-04-24 20:57:39 +0900

    Update tiling.sh

--- tiling.sh
+++ tiling.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+ORIGINAL_DIR=$(pwd)
+REPO_URL="https://github.com/disintegrating8/dotfiles"
+REPO_NAME="dotfiles"
+
 echo "Installing Dependencies"
 install_packages "${TILING[@]}"
 
@@ -7,7 +13,7 @@ curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.
 (git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
 
 echo "Cloning Config"
-git clone https://github.com/FelixKratz/dotfiles.git
+git clone "$REPO_URL"
 #mv $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
 #mv /tmp/dotfiles/.config/sketchybar $HOME/.config/sketchybar
 #rm -rf /tmp/dotfiles
